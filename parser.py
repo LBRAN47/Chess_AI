94398a094b2a69f23932715880b4686f93b0d2de
LBRAN47 <lukebrandon2004@gmail.com>
2025-04-25 10:00:14 +1000

    Regular moves work (w/ double disambiguate)

--- parser.py
+++ parser.py
@@ -18,14 +18,14 @@ class Parser():
             return
         col = coord[0]
         try:
-            row = int(coord[1]) - 1
+            row = int(coord[1])
         except ValueError:
             print("ValueError")
             return
         if col not in self.COLUMNS or row not in self.ROWS:
             print("Invalid Coordinate")
             return
-        return (self.COLUMN_CONVERT[col], row)
+        return (self.COLUMN_CONVERT[col], row-1)
 
 
     def parse_move(self, move, board):
@@ -33,6 +33,7 @@ class Parser():
 
         parameters:
             move : String in the form of Standard Chess Notation
+            board: Instance of the class Board
 
         Returns: two sets of coordinates, the first being the square to move from
         and the second to move to."""
@@ -48,16 +49,47 @@ class Parser():
         if move[0] == '0' or move[0] == 'O':
             return self.parse_castle(move, board)
 
-
-
         if move[0] in self.COLUMNS:
             return self.parse_pawn(move, board)
 
-        elif move[0] in self.PIECES:
-            if move[1] == 'x':
-                return self.parse_captures(move, board)
-            
+        elif move[0] in self.PIECES: #Basic Move
+            if len(move) < 3:
+                print(f"invalid length notation {move}")
+                return
+            piece_str, move = move[0], move[1:] #cut off processed characters
+            row, col = None, None
+            if move[0].isdigit() and int(move[0]) in self.ROWS:
+                row, move = int(move[0]) - 1, move[1:] 
+            elif move[0] in self.COLUMNS:
+                col, move = self.COLUMN_CONVERT[move[0]], move[1:]
+                if move[0].isdigit() and int(move[0]) in self.ROWS:
+                    row, move = int(move[0]) - 1, move[1:]
+                    if len(move) == 0: #basic move e.g. Be4
+                        return self.get_moveset(piece_str, (col, row), board)
 
+            if len(move) != 0 and move[0] == 'x':
+                move = move[1:]
+
+            start_coords = col, row
+
+            if len(move) != 2:
+                print("invalid length notation")
+                return
+            if move[0] not in self.COLUMNS:
+                print("Invalid Notation: incorrect coordinate")
+                return
+            target_col = self.COLUMN_CONVERT[move[0]]
+            try:
+                target_row = int(move[1])
+            except ValueError:
+                print("Expected number in move")
+                return
+            if target_row not in self.ROWS:
+                print("Invalid Notation: incorrect coordinate")
+                return
+
+            target_coords = (target_col, target_row - 1)
+            return self.get_moveset(piece_str, target_coords, board, start_coords)
 
         
     def parse_pawn(self, move, board):
@@ -85,6 +117,8 @@ class Parser():
             valid_length = 6
         else:
             ending_square = self.convert_coordinate(move)
+            if ending_square is None:
+                return
             for i in [-2, -1, 1, 2]:
                 coord = (start_col, ending_square[1]+i)
                 if not isinstance(board.get_square(coord), Pawn):
@@ -112,6 +146,16 @@ class Parser():
             return
                 
 
+    def get_moveset(self, piece_str, target_coords, board, start_coords=None):
+        """return a triple of a starting coordinate, target_coordinate and None"""
+        if start_coords is not None:
+            start_col, start_row = start_coords
+            if start_col is not None and start_row is not None:
+                square = board.get_square(start_coords) 
+                if square is None or piece_str != str(square):
+                    print(f"no piece {piece_str} at starting coordinates {start_coords}")
+                    return
+                return (start_coords, target_coords, None)
             
 
 
